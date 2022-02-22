256bf9b89275a266e1d45133a55a06f4834b44a9
adwait kulkarni <adwaitkulkarni2211@gmail.com>
2022-02-22 15:19:56 +0530

    static navbar

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import './signup.css'
 import './login.css'
 // import 'pure-react-carousel/dist/react-carousel.es.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "../components/Feed.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
